81543232dc21326368c670c5b86325a145ced828
Sebastian Wade <wade.s@etoncollege.org.uk>
2019-11-05 12:18:05 +0000

    Initial commit of project files

---
+++ StandardAlgorithms/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  StandardAlgorithms
+//
+//  Created by Wade, Sebastian (WING) on 05/11/2019.
+//  Copyright © 2019 Wade, Sebastian (WING). All rights reserved.
+//
+
+import Foundation
+
+print ("hey bro.")
+print("Hello, World!")
+
+for i in 1...3 {
+    print (i)
+}
